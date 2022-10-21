67ef2e130b18e6c0fa6a208ae2249da297f48e39
Byron-Hodel <byron@hodel.com>
2022-10-21 10:51:01 -0400

    added iteration counter

--- testing/main.c
+++ testing/main.c
@@ -20,16 +20,16 @@ int main(void) {
 
 	platform_window_t* window = platform_create_window(platform_context, create_info, NULL);
 
+	uint32_t i = 0;
 	while(1) {
-		platform_handle_events();
+		platform_handle_events(platform_context);
+		char buffer[32] = {0};
+		sprintf(buffer, "\rIteration: %d", i++);
+		platform_terminal_print(buffer, PLATFORM_COLOR_BLUE, 0, PLATFORM_TEXT_BOLD);
 		platform_sleep_miliseconds(16);
 	}
 
 	platform_destroy_window(platform_context, window, NULL);
-
-
-	platform_terminal_print("Created Platform Context.\n", PLATFORM_COLOR_GREEN, 0, 0);
-
 	platform_destroy_context(platform_context, NULL);
 
 	return 0;
